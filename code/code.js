d3da6f5f6947d3ac50ef853a17eadccda11e23e2
nikitash98 <nikitash98@gmail.com>
2022-08-11 00:47:52 -0400

    new changes

--- code/code.js
+++ code/code.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     console.log("HELLO")
     $('.more-info').hide();
 
+    /*
     $(".slice-map-text").hover(
         function(e){
             //$(".map-container-info").show(200);
@@ -14,8 +15,6 @@ $(document).ready(function() {
                     $(".map-container-info > h1").text(information[8].title)
                     $(".map-container-info>img").attr('src', information[8].img)
                     $(".map-container-info>p").text(information[8].caption)
-
-
                     break;
                 case "text_02":
                     $(".map-container-info > h1").text(information[7].title)
@@ -46,6 +45,17 @@ $(document).ready(function() {
             console.log("out")
         }
     )
+    */
+
+    $("#hide-terms").click(function(e){
+        $(".term-box-words").slideToggle( 'slow', function(){
+            var text = $("#hide-terms").text();
+            $("#hide-terms").text(
+                text == "Show" ? "Hide" : "Show");
+
+         });
+
+    })
 
     $(".map-effect").hover(
         function(e){
@@ -56,51 +66,75 @@ $(document).ready(function() {
 
                 $(".map-container-info").slideDown(100);
             }
-
             switch(e.target.id) {
                 case "area_01":
-                    $("#slice-map").attr('src', "Images/WebMap_V01/near.png")
-                    $(".map-container-info > h1").text(information[1].title)
-                    $(".map-container-info>img").attr('src', information[1].img)
-                    $(".map-container-info>p").text(information[1].caption)
+                    $("#slice-map").attr('src', "Images/WebMap_V02/near.png")
+                    $(".map-container-info > h1").text(information[4].title)
+                    $(".map-container-info>img").attr('src', information[4].img)
+                    $(".map-container-info>p").text(information[4].caption)
 
                     break;
                 case "area_02":
-                    $("#slice-map").attr('src', "Images/WebMap_V01/red.png")
-                    $(".map-container-info > h1").text(information[2].title)
-                    $(".map-container-info>img").attr('src', information[2].img)
-                    $(".map-container-info>p").text(information[2].caption)
-
-                    break;
-                case "area_03":
-                    $("#slice-map").attr('src', "Images/WebMap_V01/quasars.png")
+                    $("#slice-map").attr('src', "Images/WebMap_V02/red.png")
                     $(".map-container-info > h1").text(information[3].title)
                     $(".map-container-info>img").attr('src', information[3].img)
                     $(".map-container-info>p").text(information[3].caption)
 
                     break;
-                case "area_04":
-                    $("#slice-map").attr('src', "Images/WebMap_V01/cmb.png")
-                    $(".map-container-info > h1").text(information[4].title)
-                    $(".map-container-info>img").attr('src', information[4].img)
-                    $(".map-container-info>p").text(information[4].caption)
+                case "area_03":
+                    $("#slice-map").attr('src', "Images/WebMap_V02/quasars.png")
+                    $(".map-container-info > h1").text(information[2].title)
+                    $(".map-container-info>img").attr('src', information[2].img)
+                    $(".map-container-info>p").text(information[2].caption)
 
+                    break;
+                case "area_04":
+                    $("#slice-map").attr('src', "Images/WebMap_V02/cmb.png")
+                    $(".map-container-info > h1").text(information[1].title)
+                    $(".map-container-info>img").attr('src', information[1].img)
+                    $(".map-container-info>p").text(information[1].caption)
                     break;
                 case "area_05":
-                    $("#slice-map").attr('src', "Images/WebMap_V01/galaxies.png")
-                    $(".map-container-info > h1").text(information[4].title)
-                    $(".map-container-info>img").attr('src', information[4].img)
-                    $(".map-container-info>p").text(information[4].caption)
-
+                    $("#slice-map").attr('src', "Images/WebMap_V02/galaxies.png")
+                    $(".map-container-info > h1").text(information[9].title)
+                    $(".map-container-info>img").attr('src', information[9].img)
+                    $(".map-container-info>p").text(information[9].caption)
                     break;
 
+                case "axis_01":
+                    $(".angle-on-sky-axis").addClass('highlighted-axis');
+                    $(".map-container-info > h1").text(information[7].title)
+                    $(".map-container-info>img").attr('src', information[7].img)
+                    $(".map-container-info>p").text(information[7].caption)
+                    break;
+                case "axis_02":
+                    $(".redshift-axis").addClass('highlighted-axis');
+                    $(".map-container-info > h1").text(information[5].title)
+                    $(".map-container-info>img").attr('src', information[5].img)
+                    $(".map-container-info>p").text(information[5].caption)
+                    break;
+                case "axis_03":
+                    $(".lookback-time-axis").addClass('highlighted-axis');
+                    $(".map-container-info > h1").text(information[6].title)
+                    $(".map-container-info>img").attr('src', information[6].img)
+                    $(".map-container-info>p").text(information[6].caption)
+                    break;
+                case "axis_04":
+                    $(".you-are-here-axis").addClass('highlighted-axis');
+
+                    $(".map-container-info > h1").text(information[8].title)
+                    $(".map-container-info>img").attr('src', information[8].img)
+                    $(".map-container-info>p").text(information[8].caption)
+                    break;    
             }
 
             //$(".map-container-info").css('visibility', 'visible');
         
             console.log("in")
         }, function(){
-            $("#slice-map").attr('src', "Images/WebMap_V01/total.png")
+            $("#slice-map").attr('src', "Images/WebMap_V02/total.png")
+            $(".slice-map-text").removeClass('highlighted-axis');
+
             //$(".map-container-info").css('visibility', 'hidden');
             console.log("out")
         }
@@ -174,6 +208,15 @@ $(document).ready(function() {
         bottomPercentage = Math.min(bottomPercentage, 100)
         $(".banner-navigator-section").css({top: percentage + "%"})
         $(".banner-navigator-section").css({height: bottomPercentage - percentage + "%"})
+        var barPos = $(".bar_container").offset().top - $(".banner-outline").offset().top
+        var galaxyHeight = $(".banner-outline").outerHeight();
+
+        var barPercentage = barPos/galaxyHeight
+        lookback = 13.74 -  barPercentage * 13.74
+        //$("#sidebar-lookback-time").html((1.0).toFixed(2))
+        console.log("YES")
+        $("#sidebar-lookback-time").html((lookback).toFixed(1));
+        $("#sidebar-lookback-time_2").html((lookback).toFixed(1));
 
     })
     
@@ -201,6 +244,29 @@ $(document).ready(function() {
         })  
     });
 
+    $(".banner-modal").click(function(e) {
+        console.log("HELLO")
+        console.log(e.target.id)
+        switch(e.target.id){
+            case "banner-info-1":
+                $(".modal-body > img").attr('src', "Images/Skyview/Skyview_137.png")
+                break; 
+            case "banner-info-2":
+                $(".modal-body > img").attr('src', "Images/Skyview/Skyview_120.png")
+                break;   
+            case "banner-info-3":
+                $(".modal-body > img").attr('src', "Images/Skyview/Skyview_185.png")
+                break; 
+            case "banner-info-4":
+                $(".modal-body > img").attr('src', "Images/Skyview/Skyview_045.png")
+                break; 
+            case "banner-info-5":
+                $(".modal-body > img").attr('src', "Images/Skyview/Skyview_015.png")
+                break; 
+        }
+
+    })
+
 
     $("#about").click(function() {
         window.scrollTo({
@@ -209,9 +275,11 @@ $(document).ready(function() {
             behavior: 'smooth'
         })  
     })
+    /*
     $("#hide-term-box").click(function() {
         $(".term-box").hide()
     })
+    */
     $(".banner-tick").click(function() {
         var id = $(this).attr('id');
         window.scrollTo({
@@ -243,13 +311,17 @@ $(document).ready(function() {
 
 
 const information = {
-    1: {title: "The Cosmic Microwave Background", caption: "This is an actual photograph of the first flash of light emitted soon after the big bang, 13.7 billion years ago. This light has been stretched by the expansion of the Universe and arrives at us as radiowaves. This is the edge of the observable Universe.", img: "Images/Placeholder images/Example Explanations/CMB.jpeg"},
-    2: {title: "Quasars", caption: "They are massive black holes located at the center of certain galaxies. As they accrete surrounding gas, they become extremely bright and can be seen across the Universe. Their light is blueish. At these distances, galaxies have become too faint the Sloan Digital Sky Survey telescope.", img: "Images/Placeholder images/Example Explanations/Quasar.jpeg" },
+    1: {title: "The Cosmic Microwave Background", caption: "This is an actual photograph of the first flash of light emitted soon after the big bang, 13.7 billion years ago. This light has been stretched by the expansion of the Universe and arrives at us as radiowaves. This is the edge of the observable Universe.", img: "Images/Explanations/cmb_illust.png"},
+    2: {title: "Quasars", caption: "They are massive black holes located at the center of certain galaxies. As they accrete surrounding gas, they become extremely bright and can be seen across the Universe. Their light is blueish. At these distances, galaxies have become too faint the Sloan Digital Sky Survey telescope.", img: "Images/Explanations/Quasar@300x.png" },
     3: {title: "Luminous Red Galaxy", caption: "They are massive black holes located at the center of certain galaxies. As they accrete surrounding gas, they become extremely bright and can be seen across the Universe. Their light is blueish. At these distances, galaxies have become too faint the Sloan Digital Sky Survey telescope.", img: "Images/Placeholder images/Example Explanations/LRG.jpeg"},
-    4: {title: "Near Galaxies", caption: "Each dot is a galaxy. All together, they form a filamentary structure. Spiral galaxies are faint and blue. Elliptical galaxies are yellowish and much brighter. We can see them farther away.", img: "Images/Placeholder images/Example Explanations/Near Galaxies.png" },
-    5: {title: "Redshift", caption: "Texts. A test", img: "Images/Placeholder images/Example Explanations/Example Explanations/Redshift.jpeg"},
-    6: {title: "Lookback Time", caption: "Texts. <br> A test", img: "Images/Placeholder images/Example Explanations/Lookback.jpeg" },
-    7: {title: "Angle on the Sky", caption: "Texts. <br> A test", img: "Images/Placeholder images/Example Explanations/Example Explanations" },
-    8: {title: "You are Here", caption: "Texts. <br> A test", img: "Images/Placeholder images/Example Explanations/Example Explanations" },
+    4: {title: "Near Galaxies", caption: "Each dot is a galaxy. All together, they form a filamentary structure. Spiral galaxies are faint and blue. Elliptical galaxies are yellowish and much brighter. We can see them farther away.", img: "Images/Explanations/Near_placeholder.png" },
+    5: {title: "Redshift", caption: " PLAGARIZED: 'Red shift' is a key concept for astronomers. The term can be understood literally - the wavelength of the light is stretched, so the light is seen as 'shifted' towards the red part of the spectrum. Something similar happens to sound waves when a source of sound moves relative to an observer.", img: "Images/Explanations/Redshift@300x.png"},
+    6: {title: "Lookback Time", caption: "PLAGARIZED: The time elapsed between when we detect the light here on Earth and when it was originally emitted by the source, is known as the 'lookback time'. The more distant an object is from us, the further back in time we are looking. ", img: "Images/Explanations/Lookback Time@300x.png" },
+    7: {title: "Angle on the Sky", caption: "This is a slice of the sky this small... ", img: "Images/Placeholder images/Example Explanations/Example Explanations/Lookback.jpeg" },
+    8: {title: "You are Here", caption: "We are currently in the Milky Way, in the Agsastarious neighborhood, in the Stamsf arm, in the Solar System, on Planet Earth.", img: "Images/Explanations/You are Here@300x.png" },
+    9: {title: "Galaxies", caption: "PLAGARAIZED: Galaxies are sprawling systems of dust, gas, dark matter, and anywhere from a million to a trillion stars that are held together by gravity. Nearly all large galaxies are thought to also contain supermassive black holes at their centers.", img: "Images/Explanations/Galaxies_wikipedia cropped.png" },
+}
 
+const modal_info = {
+    1: {img: "Images/SkyView"}
 }
